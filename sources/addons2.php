cecc8cbd1055bc9bc3ccad006ee1ba2051c8523e
Lovinity <xanaftp@gmail.com>
2025-09-25 16:49:30 -0400

    Treat missing feature dependencies as a warning, not a dependency that must be installed

--- sources/addons2.php
+++ sources/addons2.php
@@ -709,7 +709,8 @@ function inform_about_addon_install(string $file, array $also_uninstalling = [],
             continue;
         }
         if (!has_feature($dependency)) {
-            $dependencies[] = $dependency;
+            //$dependencies[] = $dependency; // Cannot be installed because it's a feature, not an addon. So treat as a warning instead.
+            $warnings->attach(do_template('ADDON_INSTALL_WARNING', ['_GUID' => 'TODO', 'WARNING' => do_lang_tempcode('ADDON_WARNING_MISSING_DEPENDENCIES', $dependency, escape_html($file))]));
         }
     }
     $_dependencies_str = new Tempcode();
@@ -1345,7 +1346,8 @@ function inform_about_addon_upgrade(string $addon_name, array $also_uninstalling
             continue;
         }
         if (!has_feature($dependency)) {
-            $dependencies[] = $dependency;
+            //$dependencies[] = $dependency; // Cannot be installed because it's a feature, not an addon. So treat as a warning instead.
+            $warnings->attach(do_template('ADDON_INSTALL_WARNING', ['_GUID' => 'TODO', 'WARNING' => do_lang_tempcode('ADDON_WARNING_MISSING_DEPENDENCIES', $dependency, escape_html($addon_name))]));
         }
     }
     $_dependencies_str = new Tempcode();
