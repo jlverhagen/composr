2243f605988f77b7e40832ebb1f4f0216ba89f22
Lovinity <xanaftp@gmail.com>
2025-11-17 09:42:51 -0500

    Must handle null (error) return for URL in code renderer

--- sources/hooks/systems/media_rendering/code.php
+++ sources/hooks/systems/media_rendering/code.php
@@ -111,7 +111,11 @@ class Hook_media_rendering_code
         if (url_is_local($url)) {
             $url = get_custom_base_url() . '/' . $url;
         }
+
         $file_contents = http_get_contents($url, ['convert_to_internal_encoding' => true, 'trigger_error' => false, 'byte_limit' => 1024 * 1024 * 20/*reasonable limit*/]);
+        if ($file_contents === null) {
+            $file_contents = '';
+        }
 
         require_code('files');
         require_code('comcode_renderer');
