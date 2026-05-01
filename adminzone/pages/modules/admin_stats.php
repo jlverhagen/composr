b461df765d65e5db3b9e06c6e4c964b7f8263a36
Patrick Schmalstig <admin@pdstig.me>
2026-05-01 16:49:39 -0400

    Unlock stats module (for now)

--- adminzone/pages/modules/admin_stats.php
+++ adminzone/pages/modules/admin_stats.php
@@ -60,7 +60,7 @@ class Module_admin_stats extends Source_standard_crud_module
         $info['hacked_by'] = null;
         $info['hack_version'] = null;
         $info['version'] = 12;
-        $info['locked'] = true;
+        //$info['locked'] = true;
         $info['update_require_upgrade'] = true;
         $info['min_cms_version'] = 11.0;
         $info['addon'] = 'stats';
